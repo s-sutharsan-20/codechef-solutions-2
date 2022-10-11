433c78a1fcc45b12e548f743bc95cae239558023
Sutharsan.S <s.sutharsan.201104@gmail.com>
2022-10-11 19:31:49 +0530

    Add files via upload

---
+++ CHEFPAGES.py
@@ -0,0 +1,9 @@
+# cook your code here
+a,b=list(map(int,raw_input().strip().split()))
+if (a==1 and b==1):
+     print("https://discuss.codechef.com")
+elif (a==1 and b==0):
+     print("https://www.codechef.com/contests")
+else:
+     print("https://www.codechef.com/practice")
+    
